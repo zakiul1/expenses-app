973b7bb734df4af54d0e913f07730d9d9ce83597
zakiul1 <86553317+zakiul1@users.noreply.github.com>
2023-11-11 09:41:42 +0600

    Your commit message here

--- resources/views/pages/expenses/transaction/transaction.blade.php
+++ resources/views/pages/expenses/transaction/transaction.blade.php
@@ -78,7 +78,7 @@
 
                         </div>
                     </form>
-                    0
+
                     <button type="button" data-modal-toggle="transactionAddModal"
                         class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <svg class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20"
